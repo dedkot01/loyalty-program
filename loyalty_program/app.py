afc282b2172349c914a61b2614a95e5a75a0091e
dedkot01 <dedkot01@gmail.com>
2022-08-20 16:30:11 +0300

    added true db items

--- loyalty_program/app.py
+++ loyalty_program/app.py
@@ -1,8 +1,8 @@
 from database import db_session, init_db
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-from models import User
+from models import LoyaltyProgram, User
 
 app = Flask(__name__)
 
@@ -27,6 +27,27 @@ def loyalty_program():
     users: list = db_session.query(User).all()
 
     return render_template('administrator/loyalty_program.html', users=users)
+
+
+@app.route('/administrator/loyalty_program', methods=['POST'])
+def loyalty_program_post():
+    last_name = request.form.get('last_name')
+    first_name = request.form.get('first_name')
+    phone = request.form.get('phone')
+
+    u = User.query.filter(User.last_name == last_name,
+                          User.first_name == first_name,
+                          User.phone == phone,).first()
+
+    if u is None:
+        u = User(last_name, first_name, phone)
+        u.loyalty_program = LoyaltyProgram()
+    else:
+        u.loyalty_program.count += 1
+    db_session.add(u)
+    db_session.commit()
+
+    return str(u.loyalty_program.count)
 
 
 def main(
--- loyalty_program/models.py
+++ loyalty_program/models.py
@@ -1,14 +1,38 @@
 from database import Base
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    phone = Column(String(120), unique=True)
+    last_name = Column(String(50), unique=False)
+    first_name = Column(String(50), unique=False)
+    phone = Column(String(120), unique=False)
+
+    loyalty_program = relationship('LoyaltyProgram', back_populates='users', uselist=False)
+
+    def __init__(self, last_name: str, first_name: str, phone: str) -> None:
+        self.last_name = last_name
+        self.first_name = first_name
+        self.phone = phone
 
     def __repr__(self):
-        return f'<User {self.name!r}>'
+        return f'<User {self.last_name!r} {self.first_name!r} - {self.phone!r}>'
+
+
+class LoyaltyProgram(Base):
+    __tablename__ = 'loyalty_program'
+
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    count = Column(Integer, unique=False)
+
+    users = relationship('User', back_populates='loyalty_program')
+
+    def __init__(self, count: int = 1) -> None:
+        self.count = count
+
+    def __repr__(self):
+        return f'<LoyaltyProgram {self.count!r}>'
